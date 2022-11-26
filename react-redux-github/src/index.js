14056974f521d56d1b5656284b8a16c4e1b9bdbb
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-11-26 17:58:12 +0530

    Added: Comments in index file

--- react-redux-github/src/index.js
+++ react-redux-github/src/index.js
@@ -9,7 +9,7 @@ import {store} from "./redux/store"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Provider store={store}>
+  <Provider store={store}> {/*passing store as a props, so we don't need to import store in each and every component*/}
     <App />
   </Provider>
     
