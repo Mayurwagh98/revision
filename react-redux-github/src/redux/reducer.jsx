e2d9937617fd4426625ee28f86a317a78be99fa8
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-11-26 17:52:02 +0530

    Added: Comments in reducer file

--- react-redux-github/src/redux/reducer.jsx
+++ react-redux-github/src/redux/reducer.jsx
@@ -8,21 +8,24 @@ let Reducer = (state, action) => {
   switch (action.type) {
     case Get_Todos_Request:
       return {
-        ...state,
+        ...state, //making the copy of the state from the real state, so after dispatching it will be able to compare, 
+        //hence react will know that changes are done, so it will re-render
         isLoading: true,
       };
     case Get_Todos_Success:
       return {
-        ...state,
-        isLoading: false,
-        todos: action.payload,
+        ...state, //making the copy of the state from the real state, so after dispatching it will be able to compare, 
+        //hence react will know that changes are done, so it will re-render
+        isLoading: false, // by default keeping isLoading value as false
+        todos: action.payload, // once the request is successfull, then passing the todo as a value
       };
     case Get_Todos_Failure:
       return {
-        ...state,
-        isLoading: false,
-        todos: [],
-        isError: true,
+        ...state, //making the copy of the state from the real state, so after dispatching it will be able to compare, 
+        //hence react will know that changes are done, so it will re-render
+        isLoading: false, // by default keeping isLoading value as false
+        todos: [], //if request had failed to keeping the todos as empty, as request has failed to no todo will be added
+        isError: true, //as request has failed then making isError status as true
       };
     default:
       return state;
