13f9929de96c13b7b297ad5fdb8938ed4321416b
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-11-26 17:54:38 +0530

    Added: Comments in store file

--- react-redux-github/src/redux/store.jsx
+++ react-redux-github/src/redux/store.jsx
@@ -1,8 +1,8 @@
 
-import {legacy_createStore}  from "redux";
+import {legacy_createStore}  from "redux"; //importing leagacy_createSotre from redux, which will help to create the store
 import {Reducer} from "./reducer"
 
-let store = legacy_createStore(Reducer, {
+let store = legacy_createStore(Reducer, {  //creating store and passing two arguments, state (Reducer) and action (object which has todos, isLoading, isError and status)
   todos: [],
   isLoading: false,
   isError: false,
