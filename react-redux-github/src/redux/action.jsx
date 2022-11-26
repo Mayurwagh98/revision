cae0e6b9a32b823836a6844ffda2558709ac0674
Mayur Wagh <69896733+Mayurwagh98@users.noreply.github.com>
2022-11-26 17:47:22 +0530

    Added: Comments in action file

--- react-redux-github/src/redux/action.jsx
+++ react-redux-github/src/redux/action.jsx
@@ -4,39 +4,40 @@ import {
   Get_Todos_Failure,
 } from "./actionTypes";
 
-let getTodosRequest = () => {
+//action objects creators
+let getTodosRequest = () => { //function to make a get request
   return {
     type: Get_Todos_Request,
   };
 };
 
-let getTodosSuccess = (payload) => {
+let getTodosSuccess = (payload) => { //function success once the todo is got
   return {
     type: Get_Todos_Success,
     payload,
   };
 };
 
-let getTodosFailure = () => {
+let getTodosFailure = () => { //function to handle faliure
   return {
     type: Get_Todos_Failure,
   };
 };
 
-let postTodoRequest = () => {
+let postTodoRequest = () => { //functiob to make post request
   return {
     type: "Post_Todo_Request",
   };
 };
 
-let postTodoSuccess = (payload) => {
+let postTodoSuccess = (payload) => { // function to handle success post 
   return {
     type: "Post_Todo_Success",
     payload,
   };
 };
 
-let postTodoFailure = () => {
+let postTodoFailure = () => { //function to handle failure
   return {
     type: "Post_Todo_Failure",
   };
