8b6a5577473d24ef150161b39505bea3d114d0c1
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-28 00:51:29 +0530

    Added: Pages Folder

---
+++ todo_thunk/todo_thunk/src/Pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Todos } from "../Components/Todos";
+
+let Homepage = () =>{
+
+    return(
+        <>
+        <Todos />
+        </>
+    )
+}
+
+export {Homepage}
