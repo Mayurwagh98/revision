588f06cf6a7b9f5006184455d76ae4dc93b4d9ff
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-28 17:20:20 +0530

    Added: MiddleWare

--- todo_thunk/todo_thunk/src/Redux/store.jsx
+++ todo_thunk/todo_thunk/src/Redux/store.jsx
@@ -1,6 +1,15 @@
-import { legacy_createStore } from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducer";
 
-let store = legacy_createStore(reducer);
+let customMiddleWare = (store) => (next) => (action) => {
+  if (typeof action === "function") {
+    // console.log(next) //----> "next" here is a dispatch function
+    return action(store.dispatch);
+  }
+
+  return next(action); // "next" here means go the next middleware or reducer
+};
+
+let store = legacy_createStore(reducer, applyMiddleware(customMiddleWare));
 
 export { store };
