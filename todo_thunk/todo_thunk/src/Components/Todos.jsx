65ccb2361cc8f320b2f86785532712624a781230
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-28 16:53:36 +0530

    Moved: getTodos function in action file

--- todo_thunk/todo_thunk/src/Components/Todos.jsx
+++ todo_thunk/todo_thunk/src/Components/Todos.jsx
@@ -1,38 +1,20 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  getTodoRequest,
-  getTodoSuccess,
-  getTodoFailure,
-} from "../Redux/action";
+import { getTodos } from "../Redux/action";
 import { TodoInput } from "./TodoInput";
 
 let Todos = () => {
   let todos = useSelector((state) => state.todos);
   let dispatch = useDispatch();
-  let getTodos = () => {
-    dispatch(getTodoRequest());
-    axios
-      .get(`http://localhost:8080/todos`)
-      .then((res) => {
-        dispatch(getTodoSuccess(res.data));
-        // console.log(res.data);
-      })
-      .catch((e) => {
-        dispatch(getTodoFailure());
-        console.log(e);
-      });
-  };
 
   useEffect(() => {
-    getTodos();
+    getTodos(dispatch);
   }, []);
 
   return (
     <>
       <h1>Todos</h1>
-      <TodoInput getTodos={getTodos} />
+      <TodoInput />
       {todos.length > 0 &&
         todos.map((item) => {
           return (
