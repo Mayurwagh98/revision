fbd83adf875d76c735892e21a22150a89d5f93b8
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-29 22:06:37 +0530

    Added: MainRoutes in Components

---
+++ CRUD_app/CRUD_app/src/Components/MainRoutes.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { MusicRecords } from "../pages/MusicRecords";
+import { SingleMusicRecord } from "../pages/SingleMusicRecord";
+import { EditMusicRecord } from "../pages/EditMusicRecord";
+import { Login } from "../pages/Login";
+
+let MainRoutes = () => {
+  <Routes>
+    <Route path="/" element={<MusicRecords />} />
+    <Route path="/music/:id" element={<SingleMusicRecord />} />
+    <Route path="/music/:id/edit" element={<EditMusicRecord />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="*" element={<h3>Page Not Found</h3>} />
+  </Routes>;
+};
+
+export { MainRoutes };
