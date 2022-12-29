43e1556676a20a208c5d87ba7d832607c4ffefb1
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-29 22:41:52 +0530

    Returned: All Routes

--- CRUD_app/CRUD_app/src/Components/MainRoutes.jsx
+++ CRUD_app/CRUD_app/src/Components/MainRoutes.jsx
@@ -6,13 +6,17 @@ import { EditMusicRecord } from "../pages/EditMusicRecord";
 import { Login } from "../pages/Login";
 
 let MainRoutes = () => {
-  <Routes>
-    <Route path="/" element={<MusicRecords />} />
-    <Route path="/music/:id" element={<SingleMusicRecord />} />
-    <Route path="/music/:id/edit" element={<EditMusicRecord />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="*" element={<h3>Page Not Found</h3>} />
-  </Routes>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MusicRecords />} />
+        <Route path="/music/:id" element={<SingleMusicRecord />} />
+        <Route path="/music/:id/edit" element={<EditMusicRecord />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<h3>Page Not Found</h3>} />
+      </Routes>
+    </>
+  );
 };
 
 export { MainRoutes };
