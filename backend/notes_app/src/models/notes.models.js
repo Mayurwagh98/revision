9ad91157d2690ac2c1e6a0e0fb1979137c85e30a
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-04 16:05:50 +0530

    Added: patch request

--- backend/notes_app/src/models/notes.models.js
+++ backend/notes_app/src/models/notes.models.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose")
 const notesSchema = mongoose.Schema({
     title: String,
     note: String,
-    category: [],
-    userID: String
+    category: []
 })
 
 module.exports = mongoose.model("Notes", notesSchema)
