1f3044fbeaef60d1863df9f519ca3879eaf63f5c
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-07 16:43:30 +0530

    Changed: authentication to authorize

--- backend/notes_app/src/middleware/auth.js
+++ backend/notes_app/src/middleware/auth.js
@@ -1,28 +1,42 @@
 const jwt = require("jsonwebtoken")
 
-const authenticate = (req, res, next) =>{
+const authorize = (req, res, next) =>{
     // getting token from heders and splitting it because,
     // we pass token like  --> Bearer "whatever token user has"
     // so we only need token not Bearer string, so asccess token which is present on
     // index 1
-    const token = req.headers?.auth?.split(" ")[1] 
+    const token = req.headers?.authorization?.split(" ")[1] 
+    try {
+        if(token){
+            const decoded = jwt.verify(token, "hash")
+    
+            // console.log(decoded)
 
-    if(token){
-        const decoded = jwt.verify(token, "hash")
-
-        if(decoded){
-            // const userID = decoded.userID
-            // req.body.userID = userID
-            next()
+            if(decoded){
+    
+                const userID = decoded.userID // getting the user id from the token
+    
+                // adding the users user id in the body, 
+                // so that everytime user creates a post he doesn't need to pass his/her user id manually
+                req.body.userID = userID 
+    
+                next()
+            }
+            else{
+                res.send("You need to login")
+            }
+    
         }
         else{
             res.send("You need to login")
-        }
+        }    
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+   
 
-    }
-    else{
-        res.send("You need to login")
-    }
+    
+    
 }
 
-module.exports = authenticate
+module.exports = authorize
