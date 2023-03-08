fb3c249975b918154a27d5d3a00ad23756092824
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-09 02:36:26 +0530

    changed: modal

--- backend/frontend_notes_app/src/components/New_modal.jsx
+++ backend/frontend_notes_app/src/components/New_modal.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./New_modal.css";
 
-const New_modal = (props) => {
+const New_modal = ({ togglePopup, item }) => {
+ 
   return (
     <>
       <div className="popup" id="popup-1">
         <div className="overlay"></div>
         <div className="content">
-          <div className="close-btn" onClick={props.togglePopup}>
+          <div className="close-btn" onClick={togglePopup}>
             &times;
           </div>
-          <p className="userId"></p>
-          <p className="title"></p>
-          <p className="note"></p>
-          <p className="category"></p>
+          <p className="userId">UserID:- {item.userID}</p>
+          <p className="title">Title:- {item.title}</p>
+          <p className="note">Note:- {item.note}</p>
+          {/* <p className="category"></p> */}
         </div>
       </div>
     </>
