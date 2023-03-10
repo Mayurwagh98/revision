62ca34d101ed746aec98aa0950f8fe6a55b2dea7
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-10 16:45:32 +0530

    Imported: Login

--- backend/Blogs_app/backend/src/app.js
+++ backend/Blogs_app/backend/src/app.js
@@ -1,13 +1,14 @@
 const express = require("express")
+
 const app = express()
 // const cors = require("cors")
 const Signup = require("./routes/User.route")
-
+const Login = require("./routes/User.route")
 app.use(express.json())
 // app.use(cors())
 
 app.use("/api", Signup)
-
+app.use("/api", Login)
 
 
 
