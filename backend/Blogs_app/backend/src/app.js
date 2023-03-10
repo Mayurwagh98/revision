2dc538dae8c0a86c17134890259633c3c4a8082a
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-10 17:39:33 +0530

    Imported: post blog

--- backend/Blogs_app/backend/src/app.js
+++ backend/Blogs_app/backend/src/app.js
@@ -5,13 +5,14 @@ const app = express()
 const Signup = require("./routes/User.route")
 const Login = require("./routes/User.route")
 const GetBlogs = require("./routes/Blogs.route")
+const PostBlogs = require("./routes/Blogs.route")
 
 app.use(express.json())
 // app.use(cors())
 
 app.use("/api", Signup)
 app.use("/api", Login)
-app.use("/api/blogs", GetBlogs)
-
+app.use("/api", GetBlogs)
+app.use("/api", PostBlogs)
 
 module.exports = app
