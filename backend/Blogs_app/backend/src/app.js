45c5decc19381df83787bb5a5e7c289aff33966a
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-11 15:08:02 +0530

    Imported: Patch

--- backend/Blogs_app/backend/src/app.js
+++ backend/Blogs_app/backend/src/app.js
@@ -6,6 +6,7 @@ const Signup = require("./routes/User.route")
 const Login = require("./routes/User.route")
 const GetBlogs = require("./routes/Blogs.route")
 const PostBlogs = require("./routes/Blogs.route")
+const UpdateBlogs = require("./routes/Blogs.route")
 
 app.use(express.json())
 // app.use(cors())
@@ -14,5 +15,6 @@ app.use("/api", Signup)
 app.use("/api", Login)
 app.use("/api", GetBlogs)
 app.use("/api", PostBlogs)
+app.use("/api", UpdateBlogs)
 
 module.exports = app
