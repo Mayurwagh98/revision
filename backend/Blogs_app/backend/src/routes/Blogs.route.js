033ffb3a8c4a067ad342dfbde1ef543488fb09f6
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-11 15:30:30 +0530

    Added: Route for delete

--- backend/Blogs_app/backend/src/routes/Blogs.route.js
+++ backend/Blogs_app/backend/src/routes/Blogs.route.js
@@ -2,10 +2,12 @@ const router = require("express").Router()
 const {getBlogs} = require("../controller/Blogs.controller")
 const {postBlogs} = require("../controller/Blogs.controller")
 const {updateBlogs} = require("../controller/Blogs.controller")
+const {deleteBlogs} = require("../controller/Blogs.controller")
 
 router.get("/blogs", getBlogs)
 router.post("/create", postBlogs)
 router.patch("/blogs/update/:id", updateBlogs)
+router.delete("/blogs/delete/:id", deleteBlogs)
 
 
 module.exports = router
