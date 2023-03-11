087ddfccb84b57ed79ca4a2391c2d1882b985bb6
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-11 15:07:30 +0530

    Added: Route for patch req

--- backend/Blogs_app/backend/src/routes/Blogs.route.js
+++ backend/Blogs_app/backend/src/routes/Blogs.route.js
@@ -1,9 +1,11 @@
 const router = require("express").Router()
-
 const {getBlogs} = require("../controller/Blogs.controller")
 const {postBlogs} = require("../controller/Blogs.controller")
+const {updateBlogs} = require("../controller/Blogs.controller")
 
 router.get("/blogs", getBlogs)
 router.post("/create", postBlogs)
+router.patch("/blogs/update/:id", updateBlogs)
+
 
 module.exports = router
