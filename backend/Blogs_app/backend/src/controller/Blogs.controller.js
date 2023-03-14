761aae462c60e7032b1892c3ae63e382922352d7
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-14 08:51:43 +0530

    Passing: req.body

--- backend/Blogs_app/backend/src/controller/Blogs.controller.js
+++ backend/Blogs_app/backend/src/controller/Blogs.controller.js
@@ -2,22 +2,22 @@ const Blogs = require("../models/Blogs.model")
 
 // -------- Get blogs -------------------
 const getBlogs = async(req, res) =>{
-    let searchQuery = {}
-    let {title, category} = req.query
-    if(title){
-        searchQuery.title = {$regex: title}
-    }
-    else if(category){
-        searchQuery.category = {$regex: category}
-    }
-    try {
-        const blogs = await Blogs.find(searchQuery)
+    // let searchQuery = {}
+    // let {title, category} = req.query
+    // if(title){
+    //     searchQuery.title = {$regex: title}
+    // }
+    // else if(category){
+    //     searchQuery.category = {$regex: category}
+    // }
+    // try {
+        const blogs = await Blogs.find()
       
         res.status(200).send(blogs)
 
-    } catch (error) {
-        console.log({message: error.message})
-    }
+    // } catch (error) {
+        // console.log({message: error.message})
+    // }
 }
 
 // -------- Create blogs -------------------
@@ -34,7 +34,8 @@ const postBlogs = async(req, res) =>{
         }
         else{
 
-            const newBlog = await Blogs.create({title, blog_content, category})
+            const newBlog = await Blogs.create(req.body)
+            // await newBlog.save()
             res.status(200).send({message: "Blog Posted"})
         }
         // newBlog.save()
@@ -49,13 +50,19 @@ const postBlogs = async(req, res) =>{
 
 const updateBlogs = async(req, res) =>{
 
-    let blogId = req.params.id
-
-    let existingBlog = await Blogs.findOne({blogId})
+    const blogId = req.params.id
+    
+    const userID = req.body.userID 
+    
+    const existingBlog = await Blogs.findOne({_id: blogId})
+    // console.log(existingBlog)
 
     try {
         
-        if(!existingBlog){
+        if(userID !== existingBlog.userID){
+            res.status(404).send({message: "Not authorised"})
+        }
+        else if(!existingBlog){
             res.status(404).send({message: "Blogs not found"})
         }
         else{
