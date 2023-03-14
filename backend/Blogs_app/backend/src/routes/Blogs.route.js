cd64f54d8c35afff5ac7879b8b0dc538dabef52d
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-14 08:54:12 +0530

    Applied: middleware

--- backend/Blogs_app/backend/src/routes/Blogs.route.js
+++ backend/Blogs_app/backend/src/routes/Blogs.route.js
@@ -5,8 +5,8 @@ const {updateBlogs} = require("../controller/Blogs.controller")
 const {deleteBlogs} = require("../controller/Blogs.controller")
 const authorization = require("../middleware/authorization")
 
-router.get("/blogs", getBlogs)
-router.post("/blogs/create",postBlogs)
+router.get("/blogs", authorization,getBlogs)
+router.post("/blogs/create",authorization,postBlogs)
 router.patch("/blogs/update/:id", authorization,updateBlogs)
 router.delete("/blogs/delete/:id", authorization,deleteBlogs)
 
