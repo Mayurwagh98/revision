333f676daa067a70c57378066223d2a6ff879ccd
Mayurwagh98 <mswagh1998@gmail.com>
2023-03-16 21:49:22 +0530

    Imported: EditModal

--- backend/Blogs_app/frontend/src/pages/Blogs.jsx
+++ backend/Blogs_app/frontend/src/pages/Blogs.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
-
+import {EditModal} from "../components/EditModal"
 
 const Blogs = () => {
   let [blogs, setBlogs] = useState([]);
   // let { token, isLoading } = useSelector((store) => store);
   // console.log(token);
-  let cookieToken = Cookies.get("token")
-  let navigate = useNavigate()
- 
+  let cookieToken = Cookies.get("token");
+  let cookieUser = Cookies.get("user");
+  let navigate = useNavigate();
+
+  // let ls = localStorage.getItem("token")
 
   let getBlogs = async () => {
     let config = {
@@ -21,6 +23,7 @@ const Blogs = () => {
     };
 
     await axios
+      // .get("https://elk-top-coat.cyclic.app/api/blogs", config)
       .get("http://localhost:8000/api/blogs", config)
       .then((res) => {
         console.log(res.data);
@@ -33,10 +36,11 @@ const Blogs = () => {
     getBlogs();
   }, []);
 
+
   return (
     <div>
       <h1>Blogs</h1>
-      <button onClick={() => navigate('/createblogs')}>Create Blogs</button>
+      <button onClick={() => navigate("/createblogs")}>Create Blogs</button>
       <div className="main_blogs_div">
         {blogs &&
           blogs.map((item, index) => {
@@ -47,7 +51,9 @@ const Blogs = () => {
                 {item.category.map((el, index) => (
                   <p key={index}>Category:- {el}</p>
                 ))}
-                <button>Edit</button>
+                {cookieUser == item.userID ? (
+                  <EditModal item={item}/>
+                ) : null}
                 <button>Delete</button>
               </div>
             );
