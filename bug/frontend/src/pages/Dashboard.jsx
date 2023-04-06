4b5bcafbcac0969d97aad6f43f76f16ec2e498e3
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-06 12:45:44 +0530

    Sending: id as props

--- bug/frontend/src/pages/Dashboard.jsx
+++ bug/frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { AddBugs } from "../components/AddBugs";
 
 const Dashboard = () => {
   return (
     <div>
       <h1>Dasboard</h1>
-    </div>
-  )
-}
 
-export {Dashboard}
+      <AddBugs id={"critical"} />
+      <AddBugs id={"major"} />
+      <AddBugs id={"medium"} />
+      <AddBugs id={"low"} />
+    </div>
+  );
+};
+
+export { Dashboard };
