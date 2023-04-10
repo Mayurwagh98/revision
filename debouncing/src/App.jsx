cfbc4096806c92989fb360e3070786a97c75689d
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-10 17:24:28 +0530

    Fetching: Movies

--- debouncing/src/App.jsx
+++ debouncing/src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  // https://www.omdbapi.com/?apikey=e3148ffb&s=avengers
+
+  let [movies, setMovies] = useState([]);
+  let [search, setSearch] = useState("");
+
+  let getMovies = () => {
+    axios
+      .get(`https://www.omdbapi.com/?apikey=e3148ffb&s=${search}`)
+      .then((res) => {
+        console.log(res.data.Search);
+        setMovies(res.data);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  let handleSearch = (e) => {
+    setSearch(e.target.value);
+    getMovies();
+  };
+
+  return (
+    <div className="App">
+      <h1>Movies</h1>
+      <input type="text" placeholder="search movies" onChange={handleSearch} />
+     
+    </div>
+  );
 }
 
 export default App;
