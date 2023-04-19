ff42cdab87261f958a22d4a41643cea00140bd52
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-19 23:01:47 +0530

    Mapped: random words

--- word_game/frontend/src/pages/Playzone.jsx
+++ word_game/frontend/src/pages/Playzone.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Keyboard } from "../components/Keyboard";
 
 const Playzone = () => {
+  let [words, setWords] = useState([]);
+
+  let getWords = async () => {
+    axios
+      .get("http://localhost:8080/api/words")
+      .then((res) => {
+        console.log(res.data[0].words);
+        setWords(res.data[0].words);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  useEffect(() => {
+    getWords();
+  }, []);
+
+  let randomIndex = Math.floor(Math.random() * words.length);
+
   return (
     <div>
-      <h1>playzone</h1>
+      <h1>{words[randomIndex]}</h1>
+
+      <Keyboard />
     </div>
   );
 };
