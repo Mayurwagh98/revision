e39af0ddd8d0d7fe5fbb2f7547117c427b0e54a9
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-20 00:32:36 +0530

    Sending: data from child to parent

---
+++ word_game/frontend/src/components/Keyboard.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./Keyboard.css";
+
+const Keyboard = ({ randomWord, onChildData }) => {
+  const [value, setValue] = useState("");
+  let word = "";
+  const [count, setCount] = useState(0);
+
+  const handleKeyPress = (key) => {
+    setValue(value + key);
+    word += value + key;
+
+    if (randomWord.length == word.length) {
+      if (word === randomWord) {
+        console.log("true");
+      } else if (word !== randomWord) {
+        console.log("false");
+      }
+    }
+  };
+
+  const handleBackspace = () => {
+    setValue(value.slice(0, -1));
+  };
+
+  const keys = [
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    ["z", "x", "c", "v", "b", "n", "m"],
+  ];
+
+  return (
+    <div className="keyboard">
+      <button onClick={() => onChildData(value)}>Send Data to Parent</button>
+      <input type="text" value={value} placeholder="Enter word here" />
+      {keys.map((row, index) => (
+        <div key={index} className="keyboard_row">
+          {row.map((key) => (
+            <button key={key} onClick={() => handleKeyPress(key)}>
+              {key}
+            </button>
+          ))}
+        </div>
+      ))}
+      <button onClick={handleBackspace} className="backspace">
+        Backspace
+      </button>
+    </div>
+  );
+};
+
+export { Keyboard };
