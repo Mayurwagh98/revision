bfa42137ec9e65dee06bdf97870effd0a1dfc198
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-25 21:27:29 +0530

    Randered: random numbers

--- number_game/src/pages/Play.jsx
+++ number_game/src/pages/Play.jsx
@@ -4,9 +4,29 @@ import { getUsers } from "../redux/action";
 
 const Play = () => {
   let { users } = useSelector((store) => store);
-  console.log(users);
+  // console.log(users);
+  // let dispatch = useDispatch();
 
-  let dispatch = useDispatch();
+  let number;
+  if (users) {
+    if (users.difficulty == "easy") {
+      number = 5;
+    } else if (users.difficulty == "medium") {
+      number = 7;
+    } else if (users.difficulty == "hard") {
+      number = 10;
+    }
+  }
+  // create an array of 5 random numbers between 0 and 1
+  let randomNumbers = Array.from(
+    { length: number },
+    () => Math.round(Math.random() * 100) + " "
+  );
+  console.log(randomNumbers);
+
+  // create an array of 7 random numbers between 1 and 100
+  // let randomNumbers2 = new Array(7).fill().map(() => Math.floor(Math.random() * 100) + 1);
+  // console.log(randomNumbers2);
 
   // useEffect(() => {
   //   dispatch(getUsers);
@@ -15,6 +35,7 @@ const Play = () => {
   return (
     <div>
       <h2>Play</h2>
+      <div>{randomNumbers}</div>
     </div>
   );
 };
