5394de280ebacef83a79b6c1fbdc6d0aa328d029
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-25 19:14:22 +0530

    Fetching User from redux store

--- number_game/src/pages/Play.jsx
+++ number_game/src/pages/Play.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from "../redux/action";
 
 const Play = () => {
+  let { users } = useSelector((store) => store);
+  console.log(users);
+
+  let dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(getUsers);
+  // }, []);
+
   return (
     <div>
       <h2>Play</h2>
