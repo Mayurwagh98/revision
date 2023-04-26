9ef3df220f3925c6a2e69f2f0a4e3b5b7edd3b18
Mayurwagh98 <mswagh1998@gmail.com>
2023-04-26 06:43:12 +0530

    Updated: Timer component

--- number_game/src/components/Timer.jsx
+++ number_game/src/components/Timer.jsx
@@ -4,29 +4,50 @@ const Timer = ({ difficulty }) => {
   let [counter, setCounter] = useState(0);
 
   useEffect(() => {
+    let id;
     if (difficulty) {
       if (difficulty == "easy") {
         setCounter(60);
-        setInterval(() => {
+        id = setInterval(() => {
           setCounter((prev) => prev - 1);
         }, 1000);
+        setTimeout(() => {
+          clearInterval(id);
+        }, 60000);
       } else if (difficulty == "medium") {
         setCounter(40);
-        setInterval(() => {
+        id = setInterval(() => {
           setCounter((prev) => prev - 1);
         }, 1000);
+        setTimeout(() => {
+          clearInterval(id);
+        }, 40000);
       } else if (difficulty == "hard") {
         setCounter(30);
-        setInterval(() => {
+        id = setInterval(() => {
           setCounter((prev) => prev - 1);
         }, 1000);
+
+        setTimeout(() => {
+          clearInterval(id);
+        }, 30000);
+      } else {
+        setCounter(0);
       }
     }
   }, []);
 
+  let handleFinish = () => {
+    if (counter !== 0) {
+      console.log("you won");
+    } else if (counter == 0) {
+      console.log("you lose");
+    }
+  };
   return (
     <div>
       <h1>Timer:- {counter}</h1>
+      <button onClick={handleFinish}>Finish</button>
     </div>
   );
 };
