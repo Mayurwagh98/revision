65bb6fdba676adc4ac24fe8a47f7e5da806535c7
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-08 17:36:48 +0530

    hashing: password

--- backend/Social_media_app/src/controllers/User.controller.js
+++ backend/Social_media_app/src/controllers/User.controller.js
@@ -1,10 +1,11 @@
-const User = require("../models/User.model")
+const Users = require("../models/User.model")
+const bcrypt = require("bcrypt")
 
 let getUsers = async(req, res) =>{
 
     try {
         
-        let users = await User.find()
+        let users = await Users.find()
         
         return res.status(200).send(users)
 
@@ -12,8 +13,30 @@ let getUsers = async(req, res) =>{
     } catch (error) {
         return res.status(500).send({message: error.message})
     }
-
     
 }
 
-module.exports = {getUsers}
+let createUser = async(req, res) =>{
+    
+    let {name, email, password} = req.body
+
+    let exsistingUser = await Users.findOne({email})
+
+    try{
+
+        if(exsistingUser){
+            return res.status(404).send({message: "User already exists!"})
+        }
+        const hashPassword = await bcrypt.hash(password, 8)
+
+        let newUser = await Users.create({name, email, password: hashPassword})
+
+        return res.status(201).send({message:"User created", newUser})
+        
+    }
+    catch(error){
+        return res.status(500).send({message:error.message})
+    }
+}
+
+module.exports = {getUsers, createUser}
