ac203d1672f4fcc5497ff157ac8e422dc253dcbb
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-18 03:24:10 +0530

    Added:login and logout

--- backend/master_backend_6hrs/1st_project_form/index.js
+++ backend/master_backend_6hrs/1st_project_form/index.js
@@ -2,12 +2,43 @@ const express = require("express")
 const port = 5000
 const app = express()
 const mongoose = require("mongoose")
+const path = require("path")
+
 
 mongoose.connect("mongodb+srv://mayur:mayur@cluster0.ldawsar.mongodb.net/",{
     dbName:"form_nodejs"
 }).then(() => console.log("database connected")).catch((e) => console.log(e))
 
 
+app.use(express.static(path.join(path.resolve(), "public")))
+app.use(express.urlencoded({extended: true}))
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) =>{
+    res.render("login")
+})
+
+
+const userSchema = new mongoose.Schema({
+    name:String,
+    email: String
+})
+
+const Users = mongoose.model("User", userSchema)
+
+app.post("/", async(req, res) =>{
+    let {name, email} = req.body
+    
+    await Users.create({name, email})
+
+    res.redirect("logout")
+    
+})
+
+app.get("/logout",(req, res) =>{
+    res.render("logout")
+})
 
 app.listen(port, () =>{
     console.log(`listening on ${port}`)
